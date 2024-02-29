04bd7e037491fc0d491fde5359fbbe12a0a2363a
Ashok Kumar Pant <asokpant@gmail.com>
2024-02-29 13:40:36 +0545

    Fix

--- src/main/java/com/pascal/ptm/service/TicketService.java
+++ src/main/java/com/pascal/ptm/service/TicketService.java
@@ -1,13 +1,13 @@
 package com.pascal.ptm.service;
+
 import com.pascal.ptm.entities.Ticket;
 import com.pascal.ptm.repo.TicketRepo;
 
 import java.sql.SQLException;
-import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Date;
-import java.util.Objects;
 import java.util.Random;
 
 public class TicketService {
@@ -53,11 +53,10 @@ public class TicketService {
     }
 
 
-
     public boolean checkoutTicket(String ticketNumber) {
         try {
             Ticket ticket = this.getTicketByTicketNumber(ticketNumber);
-            if (ticket == null){
+            if (ticket == null) {
                 System.out.println("Ticket not found");
                 return false;
             }
@@ -65,14 +64,28 @@ public class TicketService {
             LocalDateTime exitTime = LocalDateTime.now(); // Assuming exit time is the current time
             int ticketId = ticket.getTicketId(); // Assuming you have a method to get ticket ID from Ticket object
 
-            double totalAmount = calculateTotalAmount(ticket.getEntryTime(), exitTime); // Calculate total amount
+            long durationSeconds = Duration.between(ticket.getEntryTime(), exitTime).getSeconds(); // Calculate duration in seconds
+            double totalAmount = calculateTotalAmount(durationSeconds); // Calculate total amount
             int updatedBy = 0; // Assuming you have a method to get the user ID who is checking out the ticket
 
-            return ticketRepo.saveTicketCheckoutDetail(ticketId, exitTime, updatedBy, totalAmount);
+            return ticketRepo.saveTicketCheckoutDetail(ticketId, exitTime, updatedBy, totalAmount, durationSeconds);
         } catch (SQLException e) {
             e.printStackTrace(); // Handle or log the exception appropriately
             return false; // Return false indicating failure
         }
+    }
+
+    private double calculateTotalAmount(long durationSeconds) {
+        double totalAmount = 0;
+
+        if (durationSeconds >= 60*60) { // If duration is more than or equal to an hour
+            totalAmount = Math.ceil(durationSeconds / 3600.0) * 25; // Hourly rate: 25 rs
+        } else if (durationSeconds >= 60*30) { // If duration is more than or equal to half an hour
+            totalAmount = Math.ceil(durationSeconds / 1800.0) * 15; // Half hour rate: 15 rs
+        } else if (durationSeconds >= 300) { // If duration is more than or equal to 5 minutes
+            totalAmount = 10; // Less than 5 min rate: 10 rs
+        }
+        return totalAmount;
     }
 
     // Method to calculate total amount based on entry and exit times (you can adjust the logic accordingly)
@@ -81,7 +94,6 @@ public class TicketService {
         // For example, you can calculate duration and multiply by rate
         return 0.0; // Placeholder value
     }
-
 
 
     private boolean isValidTicket(Ticket ticket) {
