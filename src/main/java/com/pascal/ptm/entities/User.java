e8166c11124c8f15de34379e3edfba84e83a0571
aishajoshi <137846404+aishajoshi@users.noreply.github.com>
2024-03-07 12:01:51 +0545

    user

--- src/main/java/com/pascal/ptm/entities/User.java
+++ src/main/java/com/pascal/ptm/entities/User.java
@@ -1,5 +1,56 @@
 package com.pascal.ptm.entities;
 
+import java.time.LocalDateTime;
+
 public class User {
+    int userId;
+    char userName;
+    String email;
+    String password;
+    LocalDateTime createdAt;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
 
 }
+
+
--- src/main/java/com/pascal/ptm/repo/UserRepo.java
+++ src/main/java/com/pascal/ptm/repo/UserRepo.java
@@ -1,7 +1,4 @@
 package com.pascal.ptm.repo;
-/*
- * Created by Ashok Kumar Pant
- * Email: asokpant@gmail.com
- * Created on 27/02/2024.
- */
 
+public class UserRepo {
+}
